1ca359d4c628fcee558bf1368a3676e38ca8883a
Shrage Smilowitz <shrage@microdivision.com>
2014-08-31 00:59:05 -0400

    Detect when file chanages and execute that file again

--- ScriptCSHost/Program.cs
+++ ScriptCSHost/Program.cs
@@ -12,15 +12,38 @@ namespace ScriptCSHost
     {
         static void Main(string[] args)
         {
-            
+            // 
+            // Setup watcher on directory 
+            // When file changes run it again 
+           
             const string fileDirectory = @"C:\app\ScriptCSHost\CSX Files\";
-            var files = Directory.GetFiles(fileDirectory, "*.csx");
+            const string fileFilter = "*.csx";
+     
+            var files = Directory.GetFiles(fileDirectory, fileFilter);
             FileManager.ExecuteFiles(files);
 
-           
+            var watcher = new FileSystemWatcher(fileDirectory, fileFilter);
+            
+            watcher.NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.FileName | NotifyFilters.LastWrite | NotifyFilters.Size;
+            watcher.Changed += watcher_Changed;
+            watcher.EnableRaisingEvents=true;
             Console.WriteLine("Done all files");
             Console.Read();
 
+            watcher.Dispose();
+
+
+        }
+
+        static void watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            var attr = File.GetAttributes(e.FullPath);
+            if (attr.HasFlag(FileAttributes.Directory))
+            {
+                return;
+            }
+            Console.WriteLine(e.FullPath + " changed");
+            FileManager.ExecuteFile(e.FullPath);
         }
 
   
