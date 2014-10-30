c43b59db68162182e55e31d2d0606dc36acc6da4
Shrage Smilowitz <shrage@microdivision.com>
2014-10-30 11:29:51 -0400

    Test

---
+++ ScriptCSServer/ITimer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptCSHost
+{
+    
+    public interface ITimer
+    {
+        void Start();
+        void Stop();
+        event EventHandler<TimerTickEventArgs> TimerTicked;
+
+
+        double Interval { get; set; }
+    }
+
+    public class TimerTickEventArgs : EventArgs
+    {
+        public DateTime TickTime { get; set; }
+    }
+
+    public class SystemTimer:ITimer
+    {
+        public System.Timers.Timer Timer { get; set; }
+        public SystemTimer()
+        {
+            Timer = new System.Timers.Timer();
+            Timer.Elapsed += timer_Elapsed;
+        }
+
+        void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (TimerTicked != null)
+            {
+                TimerTicked(this, new TimerTickEventArgs() { TickTime = e.SignalTime });
+            }
+        }
+
+        public void Start()
+        {
+            Timer.Start();
+        }
+
+        public void Stop()
+        {
+            Timer.Stop();
+        }
+
+        public event EventHandler<TimerTickEventArgs> TimerTicked;
+
+        public double Interval
+        {
+            get
+            {
+                return Timer.Interval;
+            }
+            set
+            {
+                Timer.Interval = value;
+            }
+        }
+    }
+}
---
+++ Tests/ScriptCSServerTests/Stubs/FileStub.cs
@@ -0,0 +1,36 @@
+﻿using ScriptCSHost;
+using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptCSHostTests.Stubs
+{
+    public class FileStub : CSXFile
+    {
+        public bool FileExecuted { get; set; }
+        public int ExecutionCount { get; set; }
+
+       
+        public FileStub(string fullPath)
+            : base(fullPath)
+        {
+            ExecutionCount = 0;
+        }
+
+        public FileStub(ITimer timer, IFileSystem fileSystem, string fullPath)
+            : base(timer, fileSystem, fullPath)
+        {
+            ExecutionCount = 0;
+        }
+        public override System.Diagnostics.Process Execute()
+        {
+            var p = base.Execute();
+            FileExecuted = true;
+            ExecutionCount += 1;
+            return p;
+        }
+    }
+}
